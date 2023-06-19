f830ffc32e193cfd9c0d95de6496b1df7d7acce7
HeisenBerg? <edwin_uestc@163.com>
2023-06-19 14:55:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ async def main():
       
     page.locator("div.ant-collapse-item:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > svg:nth-child(2)").click()
     counts=page.get_by_label("View Cluster").count()
-    for i range(0,counts):   
+    for i in range(0,counts):   
         topic=page.get_by_label("View Cluster").nth(i)
         topic.click()
         MONTHLY_SEARCH_VOLUME=page.locator("div.sc-jNxMLV:nth-child(1)").text_content()
@@ -25,7 +25,7 @@ async def main():
         print(f'RANKING_POTENTIAL'{RANKING_POTENTIAL})
         print(f'row_counts'{row_counts})
 
-        for i range(0,row_counts):
+        for i in range(0,row_counts):
           KEYWORDS_IN_CLUSTER =page.locator("td.ant-table-cell").nth(1)
           MSV =page.locator("td.ant-table-cell").nth(2)
           CPC =page.locator("td.ant-table-cell").nth(3)
