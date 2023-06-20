92628d333489173180d6830b34dffdec113e470a
HeisenBerg? <edwin_uestc@163.com>
2023-06-20 11:49:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,51 @@ import os
 import re
 from playwright.async_api import expect
 
+async def nologin():
+    botright_client = await botright.Botright(headless=True)
+
+    browser = await botright_client.new_browser()
+    page = await browser.new_page()
+
+    # url="https://www.linkgraph.com/content-planner-tool/"
+    # await page.goto(url)
+    # visible = await page.get_by_role('keyword').is_visible()
+    # await await page.get_by_role('keyword').fill(keyword)
+    # await await page.locator('.display-flex > button:nth-child(2)').click()
+    keyword=os.getenv('keyword')
+   
+    url="https://dashboard.linkgraph.com/content/content-planner/public?keyword="+keyword
+
+    time.sleep(120)
+    
+    locator = page.locator('#__next > div > div:nth-child(2) > div:nth-child(2) > div.sc-eZEUqN.rdECQ > div.ant-row.ant-row-no-wrap.ant-row-space-between > div:nth-child(2) > div:nth-child(1) > button.ant-btn.ant-btn-default.sc-bczRLJ.gtzvtM')
+    await expect(locator).to_contain_text("Export")
+
+    topic=await page.get_by_label("View Cluster").nth(i)
+    await topic.click()
+    MONTHLY_SEARCH_VOLUME=await page.locator("div.sc-jNxMLV:nth-child(1)").text_content()
+    TOTAL_TRAFFIC=await page.locator("div.sc-jNxMLV:nth-child(2)").text_content()
+    RANKING_POTENTIAL=await page.locator("div.sc-jNxMLV:nth-child(3)").text_content()
+    row_counts=await page.locator("tr.ant-table-row:nth-child").count()
+    print(f'MONTHLY_SEARCH_VOLUME{MONTHLY_SEARCH_VOLUME}')
+    print(f'TOTAL_TRAFFIC-{TOTAL_TRAFFIC}')
+    print(f'RANKING_POTENTIAL-{RANKING_POTENTIAL}')
+    print(f'row_counts-{row_counts}')
+
+    for i in range(0,row_counts):
+      KEYWORDS_IN_CLUSTER =await page.locator("td.ant-table-cell").nth(1)
+      MSV =await page.locator("td.ant-table-cell").nth(2)
+      CPC =await page.locator("td.ant-table-cell").nth(3)
+      print(f'KEYWORDS_IN_CLUSTER-{KEYWORDS_IN_CLUSTER}')
+      print(f'MSV-{MSV}')
+      print(f'CPC-{CPC}')
+
+          # Continue by using the Page
+    await page.goto(url)
+    visible=    await page.get_by_role("button", name="View Topics").is_visible()
+    await page.get_by_label("more").click()
+    # await page.locator(".ant-dropdown-menu-title-content > span:nth-child(1)").click()
+    await botright_client.close()
 async def main():
     botright_client = await botright.Botright(headless=True)
 
@@ -61,4 +106,4 @@ async def main():
     await botright_client.close()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(nologin())
