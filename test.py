34330ff5de2892549342a47ad38aec39646c332e
HeisenBerg? <edwin_uestc@163.com>
2023-06-20 14:54:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ async def nologin():
     print(await locator.text_content())
     await expect(locator).to_have_text("Export")
     # /html/body/div[1]/div/div/div[2]/div[2]/div[2]/div/div/div[3]/div[1]/div/div/div[1]/svg
-    await page.locator("//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div/div[3]/div[1]/div/div/div[1]/svg/path").click()
+    await page.locator('//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div/div[3]/div[1]/div/div/div[1]/svg/path').click()
     counts=await page.get_by_label("View Cluster").count()
     print('coubts',counts)
     for i in range(0,counts):   
