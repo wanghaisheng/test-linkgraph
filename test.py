0c8aac25f5f1f3f59d58a0f81a8b110c51824ce0
HeisenBerg? <edwin_uestc@163.com>
2023-06-22 18:27:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,73 +1,75 @@
 import asyncio
-import botright
+# import botright
 import time
 import os
 
-from playwright.async_api import expect
+from playwright.async_api import expect,async_playwright
 
 async def nologin():
-    botright_client = await botright.Botright(headless=True)
-
-    browser = await botright_client.new_browser()
-    page = await browser.new_page()
-
-    keyword = os.getenv('keyword')
-
-    url = "https://dashboard.linkgraph.com/content/content-planner/public?keyword=" + keyword
-    await page.goto(url)
-
-    topic_found = '.ant-row.ant-row-no-wrap.ant-row-space-between'
-    topic_found_locator = page.locator(topic_found)
-    result = await topic_found_locator.text_content()
-
-    while "Creating clusters takes up to 2 minutes" in result:
-        print('ooooo:', result)
-        print('ooooo:', result.find("Creating clusters takes up to 2 minutes") != -1  )
-        print('Still preparing, waiting another 30 seconds')
-        time.sleep(10)
-    print('didi:', result)
-
-    print('URL:',page.url)
-    try:
-        clusters_locator = page.locator('div.sc-jMFEJM.eaxCEo > div > div > div:nth-child(3) > div > div > div > div:nth-child(1) > svg')
-        if await clusters_locator.is_visible():
-            print(await clusters_locator.text_content())
-            await clusters_locator.click()
-        else:
+    async with async_playwright() as playwright:
+        botright_client = await playwright.start()    
+        # botright_client = await botright.Botright(headless=True)
+    
+        browser = await botright_client.new_browser()
+        page = await browser.new_page()
+    
+        keyword = os.getenv('keyword')
+    
+        url = "https://dashboard.linkgraph.com/content/content-planner/public?keyword=" + keyword
+        await page.goto(url)
+    
+        topic_found = '.ant-row.ant-row-no-wrap.ant-row-space-between'
+        topic_found_locator = page.locator(topic_found)
+        result = await topic_found_locator.text_content()
+    
+        while "Creating clusters takes up to 2 minutes" in result:
+            print('ooooo:', result)
+            print('ooooo:', result.find("Creating clusters takes up to 2 minutes") != -1  )
+            print('Still preparing, waiting another 30 seconds')
+            time.sleep(10)
+        print('didi:', result)
+    
+        print('URL:',page.url)
+        try:
+            clusters_locator = page.locator('div.sc-jMFEJM.eaxCEo > div > div > div:nth-child(3) > div > div > div > div:nth-child(1) > svg')
+            if await clusters_locator.is_visible():
+                print(await clusters_locator.text_content())
+                await clusters_locator.click()
+            else:
+                print('xpath is failed')
+        except:
             print('xpath is failed')
-    except:
-        print('xpath is failed')
-
-    os.makedirs(".output", exist_ok=True)
-    await page.screenshot(path="./output/1.png", full_page=True)
-
-    counts = await page.locator("div.sc-jMFEJM.eaxCEo > div > div > div.ant-collapse-item.ant-collapse-item-active.ant-collapse-no-arrow > div.ant-collapse-content.ant-collapse-content-active > div > div > div").count()
-    print('counts:', counts)
-
-    # for i in range(0,counts):   
-    #     topic=await page.get_by_role("button", name="View Cluster").nth(i)
-    #     print(f'i{i}')
-        
-    #     await topic.click()
-    #     MONTHLY_SEARCH_VOLUME=await page.locator("div.sc-jNxMLV:nth-child(1)").text_content()
-    #     TOTAL_TRAFFIC=await page.locator("div.sc-jNxMLV:nth-child(2)").text_content()
-    #     RANKING_POTENTIAL=await page.locator("div.sc-jNxMLV:nth-child(3)").text_content()
-    #     row_counts=await page.locator("tr.ant-table-row:nth-child").count()
-    #     print(f'MONTHLY_SEARCH_VOLUME{MONTHLY_SEARCH_VOLUME}')
-    #     print(f'TOTAL_TRAFFIC-{TOTAL_TRAFFIC}')
-    #     print(f'RANKING_POTENTIAL-{RANKING_POTENTIAL}')
-    #     print(f'row_counts-{row_counts}')
-
-    #     for i in range(0,row_counts):
-    #       KEYWORDS_IN_CLUSTER =await page.locator("td.ant-table-cell").nth(1)
-    #       MSV =await page.locator("td.ant-table-cell").nth(2)
-    #       CPC =await page.locator("td.ant-table-cell").nth(3)
-    #       print(f'KEYWORDS_IN_CLUSTER-{KEYWORDS_IN_CLUSTER}')
-    #       print(f'MSV-{MSV}')
-    #       print(f'CPC-{CPC}')
-
-    #       # Continue by using the Page
-    await botright_client.close()
+    
+        os.makedirs(".output", exist_ok=True)
+        await page.screenshot(path="./output/1.png", full_page=True)
+    
+        counts = await page.locator("div.sc-jMFEJM.eaxCEo > div > div > div.ant-collapse-item.ant-collapse-item-active.ant-collapse-no-arrow > div.ant-collapse-content.ant-collapse-content-active > div > div > div").count()
+        print('counts:', counts)
+    
+        # for i in range(0,counts):   
+        #     topic=await page.get_by_role("button", name="View Cluster").nth(i)
+        #     print(f'i{i}')
+            
+        #     await topic.click()
+        #     MONTHLY_SEARCH_VOLUME=await page.locator("div.sc-jNxMLV:nth-child(1)").text_content()
+        #     TOTAL_TRAFFIC=await page.locator("div.sc-jNxMLV:nth-child(2)").text_content()
+        #     RANKING_POTENTIAL=await page.locator("div.sc-jNxMLV:nth-child(3)").text_content()
+        #     row_counts=await page.locator("tr.ant-table-row:nth-child").count()
+        #     print(f'MONTHLY_SEARCH_VOLUME{MONTHLY_SEARCH_VOLUME}')
+        #     print(f'TOTAL_TRAFFIC-{TOTAL_TRAFFIC}')
+        #     print(f'RANKING_POTENTIAL-{RANKING_POTENTIAL}')
+        #     print(f'row_counts-{row_counts}')
+    
+        #     for i in range(0,row_counts):
+        #       KEYWORDS_IN_CLUSTER =await page.locator("td.ant-table-cell").nth(1)
+        #       MSV =await page.locator("td.ant-table-cell").nth(2)
+        #       CPC =await page.locator("td.ant-table-cell").nth(3)
+        #       print(f'KEYWORDS_IN_CLUSTER-{KEYWORDS_IN_CLUSTER}')
+        #       print(f'MSV-{MSV}')
+        #       print(f'CPC-{CPC}')
+    
+        #       # Continue by using the Page
+        await botright_client.close()
 
 async def main():
     botright_client = await botright.Botright(headless=True)
