4340d817ebfc5c4d4cc620d23850aa6e35695007
HeisenBerg? <edwin_uestc@163.com>
2023-06-22 21:34:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ finished="topic found for"
 
 async def not_finished(page) -> bool:
     s = await page.locator(STATUS_CONTAINER).text_content()
-    print(s)
+    print('current text:',s)
     return s.find(processing) != -1
 async def nologin():
     async with async_playwright() as playwright:
